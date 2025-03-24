5b456fb835cc2267d5d21959f017d00d3962c376
vinicius inocêncio <vinicius1silva61@gmail.com>
2025-03-24 10:09:48 -0300

    inicio das aulas de js

---
+++ Curso-dev-quest/modulos/js/03-variables/index.js
@@ -0,0 +1,7 @@
+var nomePessoa = "Carlos"
+const emailPessoa = "carlos@gmail.com"
+let telefonePessoa ="40028922"
+
+let retornoDoUsuario = confirm("Deseja continuar?")
+
+console.log(retornoDoUsuario)
---
+++ Curso-dev-quest/modulos/js/03.1-depurando-codigo/index.js
@@ -0,0 +1,3 @@
+console.log("usando o depurador");
+console.log("testando o depurador");
+console.log("confirmando o depurador");
