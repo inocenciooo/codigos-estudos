e7048fe212f2ef57fcb27316f2b33ef446ed4a19
vinicius inocêncio <vinicius1silva61@gmail.com>
2025-04-07 19:06:56 -0300

    inicio da aula

--- Curso-dev-quest/modulos/js/07-loop-repeticao/index.js
+++ Curso-dev-quest/modulos/js/07-loop-repeticao/index.js
@@ -4,6 +4,33 @@
     }
  */
 
+    // Com o for
+    // Irá repetir ater atingir a condição
+/*
 for (let i = 0; i < 5; i++) {
     console.log('O número atual é: ' + i)
 }
+*/
+
+    //Com While
+    //Enquanto for verdadeiro irá ser executado
+/*
+    while(condicao){
+        <bloco de execucao>
+    }
+*/
+
+let a = 10
+let b = 15
+
+while (a <b){
+    console.log('Incrementando a variável a:' + a)
+    a++;
+}
+
+/*
+    do{
+    <bloco de execucao>
+    }
+
+*/
