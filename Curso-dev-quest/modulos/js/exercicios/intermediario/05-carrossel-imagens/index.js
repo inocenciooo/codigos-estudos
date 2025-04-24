5b6ba720d2250c40b274322b127af65cf6dd691c
vinicius inocêncio <vinicius1silva61@gmail.com>
2025-04-24 20:28:51 -0300

    inicio do javascript

---
+++ Curso-dev-quest/modulos/js/exercicios/intermediario/05-carrossel-imagens/index.js
@@ -0,0 +1,12 @@
+let saibaMais = document.getElementById("saiba-mais");
+
+let botaoVoltar = document.getElementById("back");
+let botaoAvancar = document.getElementById("forward");
+
+let temas = ["tema-rosa", "tema-amarelo", "tema-laranja", "tema-azul"];
+
+i = 0;
+
+botaoAvancar.addEventListener("click", function(){
+    
+})
