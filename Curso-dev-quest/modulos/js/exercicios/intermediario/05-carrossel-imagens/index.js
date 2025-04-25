c46e32b37adc745d6a4a1232148df69ea3269ebe
vinicius inocêncio <vinicius1silva61@gmail.com>
2025-04-25 10:25:56 -0300

    continuação do carrossel

--- Curso-dev-quest/modulos/js/exercicios/intermediario/05-carrossel-imagens/index.js
+++ Curso-dev-quest/modulos/js/exercicios/intermediario/05-carrossel-imagens/index.js
@@ -1,12 +1,62 @@
 let saibaMais = document.getElementById("saiba-mais");
+let fundoImagem = document.querySelector("body");
 
-let botaoVoltar = document.getElementById("back");
-let botaoAvancar = document.getElementById("forward");
+let botaoVoltar = document.querySelectorAll("img")[0];
+let botaoAvancar = document.querySelectorAll("img")[1];
 
 let temas = ["tema-rosa", "tema-amarelo", "tema-laranja", "tema-azul"];
 
-i = 0;
+let imagemDeFundo = [
+  "fundo-rosa",
+  "fundo-amarelo",
+  "fundo-laranja",
+  "fundo-azul",
+];
 
-botaoAvancar.addEventListener("click", function(){
-    
-})
+i = 1;
+
+saibaMais.classList.add(temas[0]);
+fundoImagem.classList.add(imagemDeFundo[0]);
+
+botaoAvancar.addEventListener("click", function () {
+  if (i < temas.length) {
+    saibaMais.classList.replace(temas[i - 1], temas[i]);
+    fundoImagem.classList.replace(imagemDeFundo[i - 1], imagemDeFundo[i]);
+    console.log(i);
+    i++;
+  } else {
+  }
+});
+
+botaoVoltar.addEventListener("click", function () {
+  if (i > 0) {
+    saibaMais.classList.replace(temas[i], temas[i - 1]);
+  }
+});
+
+/* 
+botaoAvancar.addEventListener("click", function () {
+  if (temas[i] !== undefined) {
+    saibaMais.classList.replace(temas[i - 1], temas[i]);
+    fundoImagem.classList.replace(imagemDeFundo[i - 1], imagemDeFundo[i]);
+    i++;
+    console.log(imagemDeFundo[i]);
+  } else {
+    saibaMais.classList.remove(temas[i - 1]);
+    saibaMais.classList.add(temas[0]);
+
+    fundoImagem.classList.remove(imagemDeFundo[i - 1]);
+    fundoImagem.classList.add(imagemDeFundo[0]);
+    i = 1;
+  }
+});
+
+botaoVoltar.addEventListener("click", function () {
+  if (temas[i] > 0) {
+    saibaMais.classList.replace(temas + 1, temas[i]);
+    fundoImagem.classList.replace(imagemDeFundo[i + 1], imagemDeFundo[i]);
+    i--;
+  }
+});
+
+*/
