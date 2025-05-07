c38d8c5dec5f22e608118ce4a13ed988c9e546ec
vinicius inocêncio <vinicius1silva61@gmail.com>
2025-05-07 10:25:54 -0300

    aprendizagem de formulários

--- outros codigos/aprendendo-formulario/script.js
+++ outros codigos/aprendendo-formulario/script.js
@@ -1,1 +1,62 @@
-const inputPreenchido = document.getElementById
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+const submit = document.getElementById("submit");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  checarUsername();
+  checarPassword();
+  checarEmail();
+  checarConfirmacaoSenha();
+});
+
+function checarUsername() {
+  const usernameValue = username.value;
+
+  if (usernameValue === "") {
+    erroInput(username, "Você precisa informar um nome de usuário.");
+  } else {
+    username.parentElement.className = "form-content";
+  }
+}
+
+function checarEmail() {
+  const emailValue = email.value;
+
+  if (emailValue === "") {
+    erroInput(email, "O campo de e-mail é obrigatório.");
+  } else {
+    email.parentElement.className = "form-content";
+  }
+}
+
+function checarPassword() {
+  const passwordValue = password.value;
+
+  if (passwordValue === "") {
+    erroInput(password, "A senha não pode ficar em branco.");
+  } else if (passwordValue.length < 6) {
+    erroInput(password, "Informe uma senha com pelo menos 6 caracteres.");
+  } else {
+    password.parentElement.className = "form-content";
+  }
+}
+
+function checarConfirmacaoSenha() {
+  const confimacaoSenhaValue = passwordConfirmation.value;
+
+  if (confimacaoSenhaValue === "") {
+    erroInput(confimacaoSenhaValue, "Por favor, confirme sua senha.");
+  }
+}
+
+function erroInput(input, message) {
+  const formItem = input.parentElement;
+  const textMessage = formItem.querySelector("a");
+
+  textMessage.innerText = message;
+  formItem.className = "form-content error";
+}
