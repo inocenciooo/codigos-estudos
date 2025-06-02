07e8c683ede10ae5b5b514b4245cb270e488140e
vinicius inocêncio <vinicius1silva61@gmail.com>
2025-06-02 09:50:52 -0300

    praticando erros

--- Curso-dev-quest/modulos/js/avancado/00-treinando-conceitos/tratando-erros.js
+++ Curso-dev-quest/modulos/js/avancado/00-treinando-conceitos/tratando-erros.js
@@ -13,4 +13,45 @@ async function testar() {
   }
 }
 
-testar();
+// testar();
+
+console.log("TREINANDO MAIS ERROS");
+
+function dividir(a, b) {
+  if (b != 0) {
+    return a / b;
+  } else {
+    throw new Error("Não é possível dividir um número por 0.");
+  }
+}
+function calcular() {
+  try {
+    const resultado = dividir(133450, 400);
+    console.log(`O resultado da divisão é ${resultado}.`);
+  } catch (er) {
+    console.log("Erro capturado:", er.message);
+  }
+}
+
+// calcular();
+
+function maiorIdade(idade) {
+  return new Promise((resolve, reject) => {
+    if (idade < 18) {
+      reject(new Error("É preciso ser maior de idade."));
+    } else {
+      resolve();
+      console.log("Você é maior de idade!");
+    }
+  });
+}
+
+async function verificarIdade() {
+  try {
+    maiorIdade(17);
+  } catch (er) {
+    console.log("Erro capturado:", er.message);
+  }
+}
+
+verificarIdade();
